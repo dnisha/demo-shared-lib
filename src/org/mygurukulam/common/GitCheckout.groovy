10b02cf6441776ef160e27c4021da2a1119afcbd
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2023-12-07 13:42:56 +0530

    checkout function

--- src/org/mygurukulam/common/GitCheckout.groovy
+++ src/org/mygurukulam/common/GitCheckout.groovy
@@ -2,6 +2,6 @@ package org.mygurukulam.common
 
 class GitCheckout {
     def checkout(branch, creds, url) {
-        return git branch: "${branch}", url: "${url}"
+         git branch: "${branch}", url: "${url}"
     }
 }
