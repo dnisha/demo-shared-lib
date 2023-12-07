dd1a49b143d211ef2eb53f8a6725af44b7a89ee9
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2023-12-07 13:24:21 +0530

    checkout function

--- src/org/mygurukulam/common/GitCheckout.groovy
+++ src/org/mygurukulam/common/GitCheckout.groovy
@@ -1,9 +1,11 @@
 package org.mygurukulam.common
 
 class GitCheckout {
-    
+
     def checkout(Map config = [:]) {
+
     echo "Cloning repo - ${config.url} from branch - ${config.branch} using creds - ${config.creds}"
     git branch: "${config.branch}", credentialsId: "${config.creds}", url: "${config.url}"
-}
+    
+    }
 }
