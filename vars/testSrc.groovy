71160efb31224faee39a55319736f3cd81c1b5a3
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2023-12-07 12:23:52 +0530

    echo function

--- vars/testSrc.groovy
+++ vars/testSrc.groovy
@@ -1,9 +1,8 @@
-def call() {
+def testMessage() {
     def commonFuncs = new org.mygurukulam.CommonFuncs()
     def hiMessage = commonFuncs.sayHi()
     return hiMessage
 }
-
 def testHello() {
     def commonFuncs = new org.mygurukulam.CommonFuncs()
     def helloMessage = commonFuncs.sayHello("World")
