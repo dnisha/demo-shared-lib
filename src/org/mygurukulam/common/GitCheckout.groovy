4799e06a9f714d89635b60330f0bf710366dd00e
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2023-12-07 13:32:12 +0530

    checkout function

--- src/org/mygurukulam/common/GitCheckout.groovy
+++ src/org/mygurukulam/common/GitCheckout.groovy
@@ -2,6 +2,6 @@ package org.mygurukulam.common
 
 class GitCheckout {
     def checkout(str1, str2, str3) {
-        echo "Cloning repo - ${str1} ${str2} ${str3}"
+        return "Cloning repo - ${str1} ${str2} ${str3}"
     }
 }
