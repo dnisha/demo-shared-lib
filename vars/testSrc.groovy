9c9b40b5ac5d2ee5a2b5ec83bac819ceeb89d58c
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2023-12-07 12:07:30 +0530

    new function

--- vars/testSrc.groovy
+++ vars/testSrc.groovy
@@ -3,3 +3,9 @@ def call() {
     def hiMessage = commonFuncs.sayHi()
     return hiMessage
 }
+
+def call() {
+    def commonFuncs = new org.mygurukulam.CommonFuncs()
+    def sayHello = commonFuncs.sayHello()
+    return sayHello
+}
