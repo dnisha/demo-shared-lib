153fadbc45c515b88214aeaa8dbb271fbb0d347e
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2023-12-24 10:08:18 +0530

    added instance 2

--- src/org/mygurukulam/TerraformPipeline.groovy
+++ src/org/mygurukulam/TerraformPipeline.groovy
@@ -2,43 +2,37 @@ package org.mygurukulam
 
 import org.mygurukulam.common.GitCheckout
 
-class TerraformPipeline {
-    static def runTerraformPipeline() {
-        pipeline {
-            agent any
-
-            stages {
-                stage('Checkout') {
-                    steps {
-                        script {
-                            GitCheckout.checkout()
-                        }
-                    }
-                }
-
-                stage('Terraform Plan') {
-                    steps {
-                        // Execute 'terraform plan' command
-                    }
-                }
-
-                stage('Terraform Apply') {
-                    steps {
-                        // Execute 'terraform apply' command
-                    }
-                }
-
-                // Add more stages as needed
-            }
-
-            post {
-                success {
-                    // Cleanup or additional steps after successful execution
-                }
-                failure {
-                    // Handle failures or cleanup steps
+def runTerraformPipeline() {
+    node {
+        stage('Checkout') {
+            steps {
+                script {
+                    GitCheckout.checkout()
                 }
             }
+        }
+
+        stage('Terraform Plan') {
+            steps {
+                // Execute 'terraform plan' command
+            }
+        }
+
+        stage('Terraform Apply') {
+            steps {
+                // Execute 'terraform apply' command
+            }
+        }
+
+        // Add more stages as needed
+    }
+
+    post {
+        success {
+            // Cleanup or additional steps after successful execution
+        }
+        failure {
+            // Handle failures or cleanup steps
         }
     }
 }
--- vars/terraformCiCd.groovy
+++ vars/terraformCiCd.groovy
@@ -1,3 +1,39 @@
 def call() {
-    org.mygurukulam.TerraformPipeline.runTerraformPipeline()
+    // Your pipeline code here
+    pipeline {
+        agent any
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    script {
+                        org.mygurukulam.common.GitCheckout.checkout()
+                    }
+                }
+            }
+
+            stage('Terraform Plan') {
+                steps {
+                    // Execute 'terraform plan' command
+                }
+            }
+
+            stage('Terraform Apply') {
+                steps {
+                    // Execute 'terraform apply' command
+                }
+            }
+
+            // Add more stages as needed
+        }
+
+        post {
+            success {
+                // Cleanup or additional steps after successful execution
+            }
+            failure {
+                // Handle failures or cleanup steps
+            }
+        }
+    }
 }
