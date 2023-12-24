883c4c3c8193ca5939d54aa5671edafebcc0e822
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2023-12-24 10:10:32 +0530

    added instance 2

--- vars/terraformCiCd.groovy
+++ vars/terraformCiCd.groovy
@@ -11,29 +11,6 @@ def call() {
                     }
                 }
             }
-
-            stage('Terraform Plan') {
-                steps {
-                    // Execute 'terraform plan' command
-                }
-            }
-
-            stage('Terraform Apply') {
-                steps {
-                    // Execute 'terraform apply' command
-                }
-            }
-
-            // Add more stages as needed
-        }
-
-        post {
-            success {
-                // Cleanup or additional steps after successful execution
-            }
-            failure {
-                // Handle failures or cleanup steps
-            }
         }
     }
 }
