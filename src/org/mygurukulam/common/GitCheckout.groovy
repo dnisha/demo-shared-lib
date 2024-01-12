73b9cbb7ccb618aaff777f8fc4c7fa21ad01e0c7
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2024-01-12 10:50:29 +0530

    template commit3

--- src/org/mygurukulam/common/GitCheckout.groovy
+++ src/org/mygurukulam/common/GitCheckout.groovy
@@ -1,6 +1,6 @@
 package org.mygurukulam.common
 
-class GitCheckout {
+class GitCheckoutScript {
     def call(String url, String branch, String creds) {
         stage('Clone') {
             steps {
@@ -13,4 +13,4 @@ class GitCheckout {
     }     
 }
 
-return new GitCheckout()
+return new GitCheckoutScript()
