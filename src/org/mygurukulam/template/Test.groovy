c824236938073530ecb5e5c6d3fdb8d82653b9c6
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2024-01-12 11:08:45 +0530

    template commit9

--- src/org/mygurukulam/template/Test.groovy
+++ src/org/mygurukulam/template/Test.groovy
@@ -3,10 +3,12 @@ package org.mygurukulam.template
 import org.mygurukulam.common.*
 
 
-def call(String url, String branch, String creds) {
+def test(String url, String branch, String creds) {
 
     def gitCheckoutInstance = new org.mygurukulam.common.GitCheckout()
 
     gitCheckoutInstance.call(repoUrl, branch, "deepak-creds")
 
 }
+
+return this
