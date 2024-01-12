c6b2f0202fa9a219e459174bd9efbba12736f73e
dnishad004 <deepak.nishad.snaatak@mygurukumal.co>
2024-01-12 10:57:31 +0530

    template commit5

--- src/org/mygurukulam/common/GitCheckout.groovy
+++ src/org/mygurukulam/common/GitCheckout.groovy
@@ -3,12 +3,10 @@ package org.mygurukulam.common
 // class GitCheckoutScript {
     def call(String url, String branch, String creds) {
         stage('Clone') {
-            steps {
                 script {
                     echo "Cloning from url: ${url} of branch: ${branch} with creds: ${creds}"
                     // git branch: "${branch}", url: "${url}"
                 }
-            }
         }
     }     
 // }
